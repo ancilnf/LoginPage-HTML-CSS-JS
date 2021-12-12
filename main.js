954357ac43f543300d5a7bf0cd40e696023887c1
ancilnf <ancil.newton@outlook.com>
2021-12-12 17:39:43 +0530

    update

--- main.js
+++ main.js
@@ -27,9 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.querySelector("#login");
   const createAccountForm = document.querySelector("#createAccount");
 
-  document
-    .querySelector("#linkCreateAccount")
-    .addEventListener("click", (e) => {
+  document.querySelector("#linkCreateAccount").addEventListener("click", (e) => {
       e.preventDefault();
       loginForm.classList.add("form--hidden");
       createAccountForm.classList.remove("form--hidden");
